5518a2920bd24ad01a0c956a4ec1eb39dd05d1e0
Ivan Yurovych <yourovichivan@gmail.com>
2023-05-04 23:07:56 +0300

    Initial commit

---
+++ src/test/kotlin/com/example/flyiodemo/FlyioDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flyiodemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlyioDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
